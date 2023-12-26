90667c56abe8201fb24a005962dd05eb2a4e94f0
sharath-mohan <sharathdesignsofficial@gmail.com>
2023-12-26 23:47:02 +0530

    learning: 5.3 assosicated functions

--- 5.Structs/5.3Mehod Syntax/method-syntax/src/main.rs
+++ 5.Structs/5.3Mehod Syntax/method-syntax/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     height: i32,
     width: i32,
@@ -6,8 +7,18 @@ impl Rectangle {
     fn area(&self) -> i32 {
         self.height * self.width
     }
-    fn width(&self)->bool{
+    fn width(&self) -> bool {
         self.width > 0
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.height * self.width > other.height * other.width
+    }
+
+    fn square(dim: i32) -> Self {
+        Self {
+            width: dim,
+            height: dim,
+        }
     }
 }
 fn main() {
@@ -17,4 +28,15 @@ fn main() {
     };
     println!("the area is {}", rect.area());
     println!("the react has width ?  {}", rect.width());
+    let rect1 = Rectangle {
+        height: 3,
+        width: 3,
+    };
+    let rect2 = Rectangle {
+        height: 6,
+        width: 6,
+    };
+    println!("rect1 will fit in rect {}", rect.can_hold(&rect1));
+    println!("rect2 will fit in rect {}", rect.can_hold(&rect2));
+    println!("create square from rectangle {:?}", Rectangle::square(6));
 }
